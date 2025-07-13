2d0228915b4381cd58de64d482ebd3b90fbda4d4
MuhammadYasinSaleem <m.yaseensalim@gmail.com>
2025-07-14 01:28:26 +0500

    add cors config to allow frontend requests

--- src/app.js
+++ src/app.js
@@ -2,12 +2,19 @@ import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 import urlRoutes from './routes/url.routes.js';
+import cors from "cors"
 
 
 dotenv.config()
 
 const app=express()
 const PORT=process.env.PORT || 3000
+
+app.use(cors({
+  origin: 'http://localhost:3001', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use(express.json())
 
